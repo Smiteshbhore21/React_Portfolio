d25306a9796d56e2d91e3588c9b463aa3cdba7cf
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2025-08-08 00:37:19 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
 import resume from '../assets/Smitesh_Resume.pdf';
+import mainLogo from '../assets/logo_portfolie.png';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src="src/assets/logo_portfolie.png" alt="Logo" className="logo" ></img>
+            <img src={mainLogo} alt="Logo" className="logo" ></img>
             <ul>
                 <li><Link to="/"><button className='Navbuttons'>Work</button></Link></li>
                 <li><Link to="/about"><button className='Navbuttons'>About</button></Link></li>
